6427008a3b3ab5e8b69643757433202f67c293ce
rahmadjunianto <rahmadjunianto.rj@gmail.com>
2018-09-30 17:11:24 +0700

    7 SQLalchemy

--- web_app/app.py
+++ web_app/app.py
@@ -1,20 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, Column, String
 
 
 def create_app():
     app = Flask (__name__)
     app.config.from_pyfile('settings.py')
+    db=SQLAlchemy(app)
+
+    class Page(db.Model):
+        __tablename__ = 'page'
+        id = Column(Integer, primary_key= True)
+        content = Column(String)
+
+    class Post(db.Model):
+        __tablename__ = 'post'
+        id = Column(Integer, primary_key= True)
+        content = Column(String)
+
+    db.create_all()
 
     @app.route('/')
     def index():
-        import psycopg2
-
-        con = psycopg2.connect('dbname=flash01 user=devuser password=devpassword host=postgres')
-        cur = con.cursor()
-        cur.execute('select content from page where id=1') # mengembalikan tuple (0,1,2)
-        content = cur.fetchone()
-        con.close()
-        return render_template('index.html', TITLE = 'FLASK 01', CONTENT=content[0])
+        page = Page.query.filter_by(id=1).first()
+        return render_template('index.html', TITLE = 'FLASK 01', CONTENT=page.content)
 
 
     @app.route('/about')
